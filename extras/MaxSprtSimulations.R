16ce3e3ae8de474d519231f47f5d747108a1ef89
schuemie <schuemie@ohdsi.org>
2021-03-12 09:02:49 -0500

    Multi-look simulation for SCCS

--- extras/MaxSprtSimulations.R
+++ extras/MaxSprtSimulations.R
@@ -4,11 +4,13 @@ parameters <- data.frame(n = 10000, # Number of subjects
                          exposureRate = 0.01, 
                          backgroundOutcomeRate = 0.0001,
                          tar = 10, # Time at risk for each exposure
-                         rr = 1) # Relative risk
+                         rr = 1,  # Relative risk
+                         maxT = 100)
 
-simulate <- function(seed, parameters, method = "llr-binomial", conditionalPoisson = TRUE) {
+simulate <- function(seed, parameters, method = "llr-binomial", conditionalPoisson = TRUE, looks = 10) {
   set.seed(seed)
-  isSignal <- function(t, method, conditionalPoisson) {
+  
+  computeAtT <- function(t, conditionalPoisson) {
     exposedTime <- t - tExposure 
     exposedTime[t <  tExposure] <- 0
     exposedTime[exposedTime > parameters$tar] <- parameters$tar
@@ -20,22 +22,22 @@ simulate <- function(seed, parameters, method = "llr-binomial", conditionalPoiss
                        exposure = c(rep(1, parameters$n,), rep(0, parameters$n)),
                        stratumId = rep(1:parameters$n, 2))
     data <- data[data$time > 0, ]
-    
-    # Verification: compute crude IRR:
-    # (sum(data$outcome[data$exposure == 1]) / sum(data$time[data$exposure == 1])) / (sum(data$outcome[data$exposure == 0]) / sum(data$time[data$exposure == 0]))
-    
+  
     if (conditionalPoisson) {
       cyclopsData <- Cyclops::createCyclopsData(outcome ~ exposure + strata(stratumId) + offset(log(time)), modelType = "cpr", data = data)
     } else {
       cyclopsData <- Cyclops::createCyclopsData(outcome ~ exposure + offset(log(time)), modelType = "pr", data = data)
     }
     fit <- Cyclops::fitCyclopsModel(cyclopsData)
-    # exp(coef(fit))
-    
-    if (method == "ci") {    
-      ci <- confint(fit, "exposure", level = .90)
-      return(ci[2] > 0)
+    if (fit$return_flag != "SUCCESS") {
+      return(data.frame(logRr = NA,
+                        lb = 0,
+                        llr = 0,
+                        events = sum(data$outcome),
+                        exposedTime = sum(data$time[data$exposure]),
+                        unexposedTime = sum(data$time[!data$exposure])))
     } else {
+      ci <- confint(fit, "exposure", level = .90)
       if (coef(fit)["exposure"] > 0) {
         llNull <- Cyclops::getCyclopsProfileLogLikelihood(object = fit,
                                                           parm = "exposure",
@@ -46,69 +48,68 @@ simulate <- function(seed, parameters, method = "llr-binomial", conditionalPoiss
       } else {
         llr <- 0
       }
-      
-      if (method == "llr-binomial") {
-        totalEvents <- sum(data$outcome)
-        cv <- Sequential::CV.Binomial(N = totalEvents,
-                                      alpha = 0.05,
-                                      M = 1,
-                                      z = sum(unexposedTime) / sum(exposedTime),
-                                      GroupSizes = c(totalEvents))$cv
-        return(llr > cv)
-      } else if (method == "llr-poisson") {
-        expectedEvents <- sum(data$time[data$exposure == 1]) * (sum(data$outcome[data$exposure == 0]) / sum(data$time[data$exposure == 0]))
-        cv <- Sequential::CV.Poisson(SampleSize = expectedEvents,
-                                     alpha = 0.05,
-                                     M = 1,
-                                     GroupSizes = c(expectedEvents))
-        
-        return(llr > cv)
-      } else if (method == "llr-chisq") {
-        return(1 - pchisq(2 * llr, df = 1) < 0.1)
-      }
+      return(data.frame(logRr = coef(fit)["exposure"],
+                        lb = ci[2],
+                        llr,
+                        events = sum(data$outcome),
+                        exposedTime = sum(data$time[data$exposure]),
+                        unexposedTime = sum(data$time[!data$exposure])))
     }
-    
   }
   
   tExposure = rexp(parameters$n,  parameters$exposureRate)
   tOutcome <- rexp(parameters$n,  parameters$backgroundOutcomeRate)
-  
-  # Compress time during TAR to achieve target RR:
   idxAfterTar <- tOutcome > tExposure + (parameters$tar * parameters$rr)
   idxDuringTar <- tOutcome > tExposure & tOutcome <= tExposure + (parameters$tar * parameters$rr)
   tOutcome[idxAfterTar] <- tOutcome[idxAfterTar] - ((1 - parameters$rr) * parameters$tar)
   tOutcome[idxDuringTar] <- tExposure[idxDuringTar] + ((tOutcome[idxDuringTar] - tExposure[idxDuringTar]) / parameters$rr)
-  return(isSignal(t = 100, method = method, conditionalPoisson = conditionalPoisson))
+  
+  t <- seq(0, parameters$maxT, length.out = looks + 1)[-1]                 
+  results <- purrr::map_dfr(t, computeAtT, conditionalPoisson = conditionalPoisson)
+  
+  # Perform sequential testing
+  if (method == "ci") {    
+    return(any(results$lb > 0, na.rm = TRUE))
+  } else if (method == "llr-binomial") {
+    sampleSizeUpperLimit <- max(results$events, na.rm = TRUE)
+    if (sampleSizeUpperLimit <= 5) {
+      return(FALSE)
+    }
+    events <- results$events
+    if (looks > 1) {
+      events[2:looks] <- events[2:looks] - events[1:(looks-1)]
+      events <- events[events != 0]
+    }
+    cv <- Sequential::CV.Binomial(N = sampleSizeUpperLimit,
+                                  M = 1,
+                                  z = max(results$unexposedTime) / max(results$exposedTime),
+                                  GroupSizes = events)$cv
+    return(any(results$llr > cv, na.rm = TRUE))
+  } else if (method == "llr-chisq") {
+    return(any(1 - pchisq(2 * results$llr, df = 1) < 0.1, na.rm = TRUE))
+  }
 }
 
 # Compute type I error (probability of a signal when the null is true). Should be 0.05:
 cluster <- ParallelLogger::makeCluster(10)
 ParallelLogger::clusterRequire(cluster, "survival")
 
-# mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-poisson", conditionalPoisson = TRUE)), na.rm = TRUE)
-# 
-# mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-poisson", conditionalPoisson = FALSE)), na.rm = TRUE)
+mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-binomial", conditionalPoisson = TRUE, looks = 10)), na.rm = TRUE)
+# [1] 0.055
 
-mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-binomial", conditionalPoisson = TRUE)), na.rm = TRUE)
-# [1] 0.054
+mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-binomial", conditionalPoisson = FALSE, looks = 10)), na.rm = TRUE)
+# [1] 0.048
 
-mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-binomial", conditionalPoisson = FALSE)), na.rm = TRUE)
-# [1] 0.032
+mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "ci", conditionalPoisson = TRUE, looks = 10)), na.rm = TRUE)
+# [1] 0.136
 
-mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "ci", conditionalPoisson = TRUE)), na.rm = TRUE)
-# [1] 0.04704705
-
-mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "ci", conditionalPoisson = FALSE)), na.rm = TRUE)
-# [1] 0.04004004
+mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "ci", conditionalPoisson = FALSE, looks = 10)), na.rm = TRUE)
 
 mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-chisq", conditionalPoisson = TRUE)), na.rm = TRUE)
-# [1] 0.047
 
 mean(unlist(ParallelLogger::clusterApply(cluster, 1:1000, simulate, parameters = parameters, method = "llr-chisq", conditionalPoisson = FALSE)), na.rm = TRUE)
-# [1] 0.04
 
 ParallelLogger::stopCluster(cluster)
-# [1] 0.032
 
 
 # Cox proportional hazards regression (cohort method) -----------------------------------------------------
